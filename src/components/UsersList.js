74b02d453144547eac241d323cd2672b5ddcee03
Mohamed  ellili <mohamed.ellili@box2home.fr>
2023-12-01 07:07:41 +0100

    feat: add user

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { fetchUsers } from "../store"
+import { addUser, fetchUsers } from "../store"
 import Skeleton from "./Skeleton"
+import Button from "./Button"
 
 export default function UsersList() {
   const dispatch = useDispatch()
@@ -10,15 +11,22 @@ export default function UsersList() {
     dispatch(fetchUsers())
   }, [dispatch])
 
+  const handleUserAdd = () => {
+    dispatch(addUser())
+  }
+
   const { data, isLoading, error } = useSelector((state) => {
     return state.users
   })
+
   if (isLoading) {
     return <div><Skeleton times={6} className="h-10 w-full" /></div>
   }
+
   if (error) {
     return <div>Error while fetching</div>
   }
+
   const rendredUsers = data.map((user) => {
     return <div key={user.id} className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
@@ -26,5 +34,13 @@ export default function UsersList() {
       </div>
     </div>
   })
-  return <div>{rendredUsers}</div>
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User </Button>
+      </div>
+      {rendredUsers}
+    </div>
+  )
 }
