d990e1f8e0c1aef2c6160e9f58127ff3fb75826d
Mohamed  ellili <mohamed.ellili@box2home.fr>
2023-12-01 07:46:16 +0100

    fix: loading icon and skeleton

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -20,33 +20,29 @@ export default function UsersList() {
   const { data } = useSelector((state) => {
     return state.users
   })
-
+  let content
   if (isLoadingUsers) {
-    return <div><Skeleton times={6} className="h-10 w-full" /></div>
+    content = <div><Skeleton times={6} className="h-10 w-full" /></div>
   }
-
-  if (loadingError) {
-    return <div>Error while fetching</div>
-  }
-
-  const rendredUsers = data.map((user) => {
-    return <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
+  else if (loadingError) {
+    content = <div>Error while fetching</div>
+  } else {
+    content = data.map((user) => {
+      return <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
       </div>
-    </div>
-  })
+    })
+  }
   return (
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser
-          ? 'Creating User...' :
-          <Button onClick={handleUserAdd}>+ Add User </Button>
-        }
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User </Button>
         {creatingUserError && 'Error Creating User...'}
       </div>
-      {rendredUsers}
+      {content}
     </div>
   )
 }
