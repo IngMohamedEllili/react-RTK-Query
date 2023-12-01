dbceb53f6e311b2550184d0277e2bd535d2abe08
Mohamed  ellili <mohamed.ellili@box2home.fr>
2023-12-01 17:00:57 +0100

    feat: add albums

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,44 @@
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store"
+import AlbumsListItem from "./AlbumListItem"
+import Button from "./Button"
+import ExpandablePanel from "./ExpandablePanel"
+import Skeleton from "./Skeleton"
+
 export default function AlbumsList({ user }) {
+  const { data, isLoading, error } = useFetchAlbumsQuery(user)
+  const [addAlbum, results] = useAddAlbumMutation()
+  let content
+  if (isLoading) {
+    content = <Skeleton times={3} className='h-3 w-full' />
+  } else if (error) {
+    content = <div>
+      Error loading Albums...
+    </div>
+  } else {
+    content = data.map((album) => {
+      return <>
+        <AlbumsListItem album={album} />
+      </>
+    })
+  }
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  }
   return (
     <div>
-      Albums by {user.name}
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">
+          Albums for {user.name}
+        </h3>
+        <Button onClick={handleAddAlbum} loading={results.isLoading}>
+          + Add Album
+        </Button>
+      </div>
+      <div>
+        {content}
+      </div>
     </div>
+
   )
 }
--- src/store/thunk/addUser.js
+++ src/store/thunk/addUser.js
@@ -1,15 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { faker } from '@faker-js/faker'
+import { pause } from "../../utils";
+
 export const addUser = createAsyncThunk('user/add', async () => {
   const response = await axios.post('http://localhost:3005/users', { name: faker.person.fullName() })
   await pause(1000)
   return response.data
 })
 
-// DEV Only!!!!
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration)
-  })
-}
