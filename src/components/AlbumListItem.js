23ca495a1a2b2dbad1fb55f6d44ac5b534fc96d4
Mohamed  ellili <mohamed.ellili@box2home.fr>
2023-12-04 10:44:29 +0100

    feat: add photos layout and the actions

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -2,6 +2,7 @@ import { GoTrashcan } from "react-icons/go"
 import Button from "./Button"
 import ExpandablePanel from "./ExpandablePanel"
 import { useRemoveAlbumMutation } from "../store"
+import PhotoList from "./PhotoList"
 
 export default function AlbumsListItem({ album }) {
   const [removeAlbum, results] = useRemoveAlbumMutation()
@@ -19,7 +20,7 @@ export default function AlbumsListItem({ album }) {
 
   return (
     <ExpandablePanel key={album.id} header={header}>
-      List of photos in the album
+      <PhotoList album={album} />
     </ExpandablePanel>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 import { albumApi } from "./apis/albumApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { photoApi } from "./apis/photoApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumApi.reducerPath]: (albumApi.reducer),
+    [photoApi.reducerPath]: (photoApi.reducer)
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumApi.middleware)
+    return getDefaultMiddleware().concat([albumApi.middleware, photoApi.middleware])
   }
 })
+
 setupListeners(store.dispatch)
 
 export * from './thunk'
-export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumApi' 
+export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumApi'
+export { useAddPhotoMutation, useFetchPhotosQuery, useRemovePhotoMutation } from './apis/photoApi'
